e14e5fa67046ae7e485bed6254b767a343141a97
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-11-21 09:19:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+ import SignUpForm from './components/SignUpForm.vue';
+</script>
+
+<template>
+    <SignUpForm />
+</template>
+
+<script>
+ export default {
+
+ }
+</script>
+
+<style>
+    #app{
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    body{
+        margin: 0;
+        background: #eee;
+    }
+</style>
